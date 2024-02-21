697371f1c868afc197c825a22d51f2f7e3fcdcd1
Ramthor <ramkumar978678@gmail.com>
2024-02-21 14:02:08 +0530

    ADD:header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,33 @@ const Header = () => {
   return (
     <Nav>
       <Logo src="/images/logo.svg" />
+      <NavMenu>
+        <a>
+          <img src="/images/home-icon.svg" alt="" />
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src="/images/search-icon.svg" alt="" />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src="/images/watchlist-icon.svg" alt="" />
+          <span>WATCHLIST</span>
+        </a>
+        <a>
+          <img src="/images/original-icon.svg" alt="" />
+          <span>ORIGINALS</span>
+        </a>
+        <a>
+          <img src="/images/movie-icon.svg" alt="" />
+          <span>MOVIES</span>
+        </a>
+        <a>
+          <img src="/images/series-icon.svg" alt="" />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+      <UserImg src="/images/ram.jpg" />
     </Nav>
   );
 };
@@ -13,8 +40,58 @@ export default Header;
 const Nav = styled.nav`
   background: #090b13;
   height: 70px;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
 `;
 const Logo = styled.img`
-  display: flex;
   width: 80px;
+`;
+const NavMenu = styled.div`
+  display: flex;
+  flex: 1;
+  margin-left: 10px;
+  align-items: center;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+
+    cursor: pointer;
+    color: white;
+
+    img {
+      height: 20px;
+    }
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      position: relative;
+      &::after {
+        content: "";
+        position: absolute;
+        height: 2px;
+        background-color: white;
+        left: 0;
+        right: 0;
+        bottom: -6px;
+        opacity: 0;
+        transform-origin: left center;
+        transition: all 250ms cubic-bezier(0.22, 0.46, 0.45, 0.94) 0s;
+        transform: scaleX(0);
+      }
+    }
+    &:hover {
+      span::after {
+        transform: scaleX(1);
+        opacity: 1;
+      }
+    }
+  }
+`;
+const UserImg = styled.img`
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+  cursor: pointer;
 `;
