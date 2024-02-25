6dc8d56cb146b2dfb628d93dd6c135d2fe3eada7
Ramthor <ramkumar978678@gmail.com>
2024-02-25 16:30:09 +0530

    Add:Detailpage and routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Header from "./components/Header";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import Detail from "./components/Detail";
 function App() {
   return (
     <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/detail" element={<Detail />} />
       </Routes>
     </>
   );
